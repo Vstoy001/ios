20d21e573b041871d1de439dc1e1993c1aea44b8
Vstoy001 <vicOstoy@gmail.com>
2015-01-14 23:05:00 -0800

    add

---
+++ cards/cards/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cards
+//
+//  Created by Victor Stoyanov on 1/14/15.
+//  Copyright (c) 2015 Victor Stoyanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
